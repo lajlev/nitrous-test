3c7220aa6b1a8461b5c09011f716d1d1a11218ac
Michael Lajlev <lajlev@gmail.com>
2013-06-27 09:27:57 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lajlev::Application.config.secret_token = '2ceb24a4267b8e4b4257823229018c9968ba11f8ed9cf035a53aeb94b1eabfc687cd16092a708efdd4046c32cc12818e671ad1690c897a94fe7afea05fc5d0ef'
